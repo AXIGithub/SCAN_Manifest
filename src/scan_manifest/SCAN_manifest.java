80ee2b9419a287b96373187e634406a738832a41
ratinoAG14 <ratino@astragraphia.co.id>
2023-01-02 18:30:41 +0700

    First Init

---
+++ src/scan_manifest/SCAN_manifest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scan_manifest;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Ratino
+ */
+public class SCAN_manifest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // MainFrame mainfream = new MainFrame();
+        new MainFrame().setVisible(true);
+        
+    }
+    
+}
